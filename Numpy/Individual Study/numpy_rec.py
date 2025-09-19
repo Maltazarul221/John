5f371512f20160629f0fd809c94aa0bf3658c5eb
Maltazarul221 <makrusu80@gmail.com>
2025-09-19 19:54:34 +0100

    1

---
+++ Numpy/Individual Study/numpy_rec.py
@@ -0,0 +1,19 @@
+#Scalar
+#A scalar is a single number. In NumPy, a scalar can be created using a zero-dimensional array.
+
+import numpy as np
+
+A = np.array(8)
+print(A)
+
+#Vector
+#A vector is a one-dimensional array of numbers or a sequence. It can be represented as:
+# Row Vector
+
+A = np.array([3,6,8,1])
+print(A)
+
+#Colum Vector
+
+A = np.array([[3] , [6], [8] , [1]])
+print(A)
